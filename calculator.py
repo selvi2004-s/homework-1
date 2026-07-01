91fd1279504f9428ccc115da599262b599a91aa9
selvihambardzumyan04 <selvi_hambardzumyan@edu.aua.am>
2026-07-01 23:54:49 +0400

    add logging

--- calculator.py
+++ calculator.py
@@ -1,21 +1,32 @@
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
 def add(a, b):
+    logger.info("Adding %s + %s", a, b)
     return a + b
 
 
 def subtract(a, b):
+    logger.info("Subtracting %s - %s", a, b)
     return a - b
 
 
 def multiply(a, b):
+    logger.info("Multiplying %s * %s", a, b)
     return a * b
 
 
 def divide(a, b):
+    logger.info("Dividing %s / %s", a, b)
     return a / b
 
 
 def square_root(a):
-    return a ** 0.5
+    logger.info("Square root of %s", a)
+    return a**0.5
 
 
 if __name__ == "__main__":
