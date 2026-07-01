50098718351f0cb40d1440b20834fb5c70b7f154
selvihambardzumyan04 <selvi_hambardzumyan@edu.aua.am>
2026-07-01 22:48:19 +0400

    implement base calculator logic

---
+++ calculator.py
@@ -0,0 +1,21 @@
+def add(a, b):
+    return a + b
+
+
+def subtract(a, b):
+    return a - b
+
+
+def multiply(a, b):
+    return a * b
+
+
+def divide(a, b):
+    return a / b
+
+
+if __name__ == "__main__":
+    print("add:", add(4, 2))
+    print("subtract:", subtract(4, 2))
+    print("multiply:", multiply(4, 2))
+    print("divide:", divide(4, 2))
